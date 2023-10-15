a0f1333e3a13bf312f5a8760e74b131419cbdab7
Will Rutland <wrutland07@gmail.com>
2023-10-15 20:38:18 +0100

    Setup database

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,22 @@
 import { initializeApp } from "firebase/app";
+import {getFirestore} from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAePSfz7GW9ZoRW8BiQHQef8OsuoOKmDn0",
+  authDomain: "hunter-pcs-firebase.firebaseapp.com",
+  projectId: "hunter-pcs-firebase",
+  storageBucket: "hunter-pcs-firebase.appspot.com",
+  messagingSenderId: "229412490217",
+  appId: "1:229412490217:web:3ef37c431381fbb0bdd978",
+  measurementId: "G-D23909SXE6"
+};
+
+const app = initializeApp(firebaseConfig);
 
 export function firebaseInit() {
-    const firebaseConfig = {
-      apiKey: "AIzaSyAePSfz7GW9ZoRW8BiQHQef8OsuoOKmDn0",
-      authDomain: "hunter-pcs-firebase.firebaseapp.com",
-      projectId: "hunter-pcs-firebase",
-      storageBucket: "hunter-pcs-firebase.appspot.com",
-      messagingSenderId: "229412490217",
-      appId: "1:229412490217:web:3ef37c431381fbb0bdd978",
-      measurementId: "G-D23909SXE6"
-    };
-    
-    // Initialize Firebase
-    initializeApp(firebaseConfig);
+  initializeApp(firebaseConfig);
 };
+
+const db = getFirestore(app);
+
+export default db;
