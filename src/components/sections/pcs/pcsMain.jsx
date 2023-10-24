5c0755be16cbb4c186c0db00e8d6c2ecae01170d
Will Rutland <wrutland07@gmail.com>
2023-10-24 16:14:25 +0100

    Added custom pcs link to pcs main page

--- src/components/sections/pcs/pcsMain.jsx
+++ src/components/sections/pcs/pcsMain.jsx
@@ -1,8 +1,8 @@
 import React, {Component} from 'react';
 import SlidingButton from '../../slidingButton.jsx';
-import {doc, getDoc} from 'firebase/firestore';
-import { getFirestore } from 'firebase/firestore';
+import {doc, getDoc, getFirestore} from 'firebase/firestore';
 import { firebaseInit } from '../../../firebase.js';
+import { changePage } from '../../../index.js';
 
 firebaseInit();
 
@@ -45,6 +45,7 @@ class PcsMain extends Component {
     render() {
         return (
             <React.Fragment>
+                {/*HEADER SECTION*/}
                 <div>
                     <h1 className="alignRight">
                         Hand crafted gaming pcs, ready to go
@@ -68,6 +69,8 @@ class PcsMain extends Component {
                         </thead>
                     </table>
                 </div>
+
+                {/*PREBUILT PRODUCTS SECTION*/}
                 <div className="purpleGrey">
                     <h1>
                         For high quality pcs, you're in the right place
@@ -140,6 +143,36 @@ class PcsMain extends Component {
                                             Dominant <br/><br/>{"£ "+this.state.prices.dominant}
                                         </React.Fragment>
                                     ) : 'loading...'} />
+                                </td>
+                            </tr>
+                        </thead>
+                    </table>
+                </div>
+
+                {/*CUSTOM PCS SECTION*/}
+                <div>
+                    <h1>
+                        Design your own pc
+                    </h1>
+                    <table>
+                        <thead>
+                            <tr>
+                                <td>
+                                    <h2 className="alignLeft">
+                                        Build your dreams
+                                    </h2>
+                                    <p className="alignRight">
+                                        Using our custom pc creator, you can design the pc of your dreams right here on our website.
+                                    </p>
+                                    <button type="button" onClick={function() {changePage('customPcs')}}>
+                                        <h3>
+                                            Click here to get started
+                                        </h3>
+                                    </button>
+                                </td>
+                                <td>
+                                    <img src='https://firebasestorage.googleapis.com/v0/b/hunter-pcs-firebase.appspot.com/o/images%2FmotherboardTall.jpeg?alt=media&token=b7433866-5203-4cbb-9808-ca1d47ee1a48'
+                                    className="mainImage centered" style={{width: '85%', height: '450px', objectFit: 'cover'}} alt="loading..." />
                                 </td>
                             </tr>
                         </thead>
