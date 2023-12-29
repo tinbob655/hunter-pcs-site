38e5f5d870e4a62b4d950411e9def27e03509dc2
Will Rutland <wrutland07@gmail.com>
2023-12-29 17:50:19 +0000

    Added "in dev" message to tell a friend page

--- src/components/pages/tellAFriend/tellAFriend.jsx
+++ src/components/pages/tellAFriend/tellAFriend.jsx
@@ -1,10 +1,25 @@
 import React, {Component} from 'react';
+import GenericMarkupSection from '../../multiPageComponents/genericMarkupSection.jsx';
 
 class TellAFriend extends Component {
 
     render() {
         return (
             <React.Fragment>
+                {/*placeholder content until tell a friend system is operational (need more profits) */}
+                <div>
+                    <GenericMarkupSection
+                    headingText='Tell a Friend All About Us'
+                    subheadingText='Get your bestie a discount on any of our PCs'
+                    paragraphText={`Our tell a friend system is currently in development. Check back later to see if its out yet! For now you can browse some high quality
+                    gaming PCs whilst your friend awaits their free discount`}
+                    linkContent='Browse gaming PCs ⟶'
+                    linkDestination='/pcsMain'
+                    DontShowDividerLineBool={true}
+                    leftBool={true}
+                    imgSrc='https://firebasestorage.googleapis.com/v0/b/hunter-pcs-firebase.appspot.com/o/images%2Frounded%20skull%202.jpeg?alt=media&token=c9e0128f-15ca-4595-bc92-15618109e0de'
+                    customHeaderClassName='alignRight' />
+                </div>
             </React.Fragment>
         );
     };
