78dfd488ca76ffec69065324ddf8db7be9694026
Will Rutland <wrutland07@gmail.com>
2024-02-13 18:10:10 +0000

    Added a smart component for images from firebase

---
+++ src/components/multiPageComponents/image.jsx
@@ -0,0 +1,22 @@
+import React, {useEffect, useState} from 'react';
+import {getDownloadURL, ref, getStorage} from 'firebase/storage';
+
+export default function Image({imagePath, imageClasses, imageStyles}) {
+
+    const [URL, setURL] = useState('');
+    console.log('rendering')
+
+    useEffect(() => {
+        //get the image url
+        const storage = getStorage();
+        getDownloadURL(ref(storage, imagePath)) 
+        .then((url) => {
+            setURL(url);
+        })
+    });
+
+    //return the iamge back to main
+    return (
+        <img src={URL} className={imageClasses} style={imageStyles ? imageStyles : {}} alt="loading..." />
+    );
+};
