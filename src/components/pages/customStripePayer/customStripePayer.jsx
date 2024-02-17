c9236fb1292c172791059ce0205c44b0be067ee6
Will Rutland <wrutland07@gmail.com>
2024-02-17 15:01:33 +0000

    Added error support for "no payment amount in URL"

--- src/components/pages/customStripePayer/customStripePayer.jsx
+++ src/components/pages/customStripePayer/customStripePayer.jsx
@@ -18,28 +18,47 @@ class CustomStripePayer extends Component {
     };
 
     render() {
-        return (
-            <React.Fragment>
-                <GenericMarkupSection
-                headingText="Get your custom PC"
-                subheadingText="The final step between you and the game"
-                paragraphText="Your custom PC has been validated. The last thing to do is click the button below, and proceed through the secure checkout. After that, we'll start
-                building your dreams!"
-                imgSrc='images/image of pc.jpeg'
-                linkContent="Get your dream PC delivered straight to you ⟶"
-                linkLogic={() => {startStripeSession(this.state.amount, 'Custom PC')
-                .then(() => {this.setState({checkout: <StripeCheckout />});
-            setTimeout(() => {
-                document.getElementById('stripeCheckoutWrapper').classList.add('shown');
-            }, 100);})}}
-                DontShowDividerLineBool={true} 
-                leftBool={true}/>
 
-                <div id="stripeCheckoutWrapper" className="popupWrapper" style={{padding: '30px'}}>
-                        {this.state.checkout}
-                </div>
-            </React.Fragment>
-        );
+        //make sure the page can only be accessed if there is a payment amount
+        if (!this.state.amount) {
+            return (
+                <React.Fragment>
+                    <GenericMarkupSection
+                    headingText="Something went wrong"
+                    subheadingText="Looks like we ran into an error"
+                    paragraphText="Either our website is encountering an eror or the link used to access this page was used incorrrectly. Please try again later"
+                    imgSrc='images/rounded skull.jpeg'
+                    linkDestination="/"
+                    linkContent="Return home ⟶"
+                    leftBool={true}
+                    DontShowDividerLineBool={true} />
+                </React.Fragment>
+            )
+        }
+        else {
+            return (
+                <React.Fragment>
+                    <GenericMarkupSection
+                    headingText="Get your custom PC"
+                    subheadingText="The final step between you and the game"
+                    paragraphText="Your custom PC has been validated. The last thing to do is click the button below, and proceed through the secure checkout. After that, we'll start
+                    building your dreams!"
+                    imgSrc='images/image of pc.jpeg'
+                    linkContent="Get your dream PC delivered straight to you ⟶"
+                    linkLogic={() => {startStripeSession(this.state.amount, 'Custom PC')
+                    .then(() => {this.setState({checkout: <StripeCheckout />});
+                setTimeout(() => {
+                    document.getElementById('stripeCheckoutWrapper').classList.add('shown');
+                }, 100);})}}
+                    DontShowDividerLineBool={true} 
+                    leftBool={true}/>
+    
+                    <div id="stripeCheckoutWrapper" className="popupWrapper" style={{padding: '30px'}}>
+                            {this.state.checkout}
+                    </div>
+                </React.Fragment>
+            );
+        }
     };
 };
 
