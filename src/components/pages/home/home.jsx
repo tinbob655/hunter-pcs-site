647b4c9d243bb5c8f06bf4af9cbac00bed609a02
Will Rutland <wrutland07@gmail.com>
2024-09-24 19:19:34 +0100

    setup routes

---
+++ src/components/pages/home/home.jsx
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+
+class Home extends Component {
+
+    render() {
+        return (
+            <React.Fragment>
+                <h1>
+                    Do sint nostrud sit nostrud Lorem irure aliquip ipsum.
+                </h1>
+                <h2>
+                    Nulla consequat esse et sit sunt enim magna exercitation non tempor.
+                </h2>
+                <h3>
+                    Cupidatat duis aliqua nulla non in fugiat pariatur do veniam velit amet aute commodo ad.
+                </h3>
+                <p>
+                    Ullamco irure cillum et elit minim mollit id magna amet exercitation consequat consequat elit.
+                </p>
+            </React.Fragment>
+        );
+    };
+};
+
+export default Home;
---
+++ src/routes.jsx
@@ -0,0 +1,45 @@
+import React, {Component} from 'react';
+import {Route, Routes} from 'react-router-dom';
+
+//import all pages
+import Home from './components/pages/home/home.jsx';
+import Account from './components/pages/account/account.jsx';
+
+
+class AllRoutes extends Component {
+
+    render() {
+        return (
+            <Routes>
+                {this.getRoutes()}
+            </Routes>
+        );
+    };
+
+    getRoutes() {
+
+        //map of all pages
+        const pages = {
+            home: <Home />,
+            account: <Account />,
+        };
+        let routeHTML = [];
+
+        //make the index route
+        routeHTML.push(
+            <Route path='/' element={<Home/>}/>
+        );
+
+        //make all the other routes
+        for (let page in pages) {
+
+            routeHTML.push(
+                <Route exact path={'/'+page} element={pages[page]} />
+            );
+        };
+
+        return routeHTML;
+    };
+};
+
+export default AllRoutes;
