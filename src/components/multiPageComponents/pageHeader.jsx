fd91f2760200d8f9e51a6dedeef7c9f23c97fa36
Will Rutland <wrutland07@gmail.com>
2024-09-24 20:28:41 +0100

    added pageHeader component

---
+++ src/components/multiPageComponents/pageHeader.jsx
@@ -0,0 +1,30 @@
+import React, {Component} from 'react';
+
+class PageHeader extends Component {
+
+    constructor(props) {
+        super(props);
+
+        //state will inherit props from parent
+        this.state = {
+            heading: this.props.heading,
+            subheading: this.props.subheading,
+        };
+    };
+
+    render() {
+        return (
+            <React.Fragment>
+                <h1 className="alignLeft noVerticalSpacing" style={{marginLeft: '12.5%'}}>
+                    {this.state.heading}
+                </h1>
+                <p className="alignLeft noVerticalSpacing" style={{marginLeft: '17%'}}>
+                    {this.state.subheading}
+                </p>
+                <div className="dividerLine"></div>
+            </React.Fragment>
+        );
+    };
+};
+
+export default PageHeader;
