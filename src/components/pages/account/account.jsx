3422e437d5c5134a9484b4bf1566d1b05e327416
Will Rutland <wrutland07@gmail.com>
2024-09-24 21:42:32 +0100

    started account page

--- src/components/pages/account/account.jsx
+++ src/components/pages/account/account.jsx
@@ -1,12 +1,34 @@
 import React, {Component} from 'react';
+import PageHeader from '../../multiPageComponents/pageHeader.jsx';
+import GenericMarkupSection from '../../multiPageComponents/genericMarkupSection.jsx';
 
 class Account extends Component {
 
     render() {
         return (
             <React.Fragment>
+                <PageHeader heading="Your account" subheading="Join us" />
+                
+                {/*why you need an account section*/}
+                <div>
+                    <GenericMarkupSection
+                        heading="Make your account"
+                        paragraph="Create an account with us and unlock the full potential of Hunter PCs. We do not collect any of your personal information as privacy is one of our core values here."
+                        left={true}
+                        dividerLine={true}
+                        imagePath="images/gamingSetupWIDE2.jpeg"
+                        buttonText="Click here to sign up"
+                        buttonAction={() => {
+                            this.startLogInSignUp();
+                        }} />
+                </div>
             </React.Fragment>
         );
+    };
+
+    startLogInSignUp() {
+        
+        //method to show the login popup
     };
 };
 
