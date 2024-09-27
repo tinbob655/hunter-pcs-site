530c203005660df1b169b5619e41dc045b17d65b
Will Rutland <wrutland07@gmail.com>
2024-09-27 21:57:57 +0100

    made different account page for if a user is logged in or not

--- src/components/pages/account/account.jsx
+++ src/components/pages/account/account.jsx
@@ -2,8 +2,13 @@ import React, {Component} from 'react';
 import PageHeader from '../../multiPageComponents/pageHeader.jsx';
 import GenericMarkupSection from '../../multiPageComponents/genericMarkupSection.jsx';
 import LoginPopup from '../../multiPageComponents/popups/loginPopup.jsx';
+import AuthProvider from '../../../context/authContext.jsx';
+import {signOut} from 'firebase/auth'
+import firebaseInstance from '../../../classes/firebase.js';
 
 class Account extends Component {
+
+    static contextType = AuthProvider;
 
     constructor(props) {
         super(props);
@@ -14,28 +19,57 @@ class Account extends Component {
     };
 
     render() {
-        return (
-            <React.Fragment>
-                <PageHeader heading="Your account" subheading="Join us" />
-                
-                {/*why you need an account section*/}
-                <div>
-                    <GenericMarkupSection
-                        heading="Make your account"
-                        paragraph="Create an account with us and unlock the full potential of Hunter PCs. We do not collect any of your personal information as privacy is one of our core values here."
-                        left={true}
-                        dividerLine={true}
-                        imagePath="images/gamingSetupWIDE2.jpeg"
-                        buttonText="Click here to sign up"
-                        buttonAction={() => {
-                            this.startLogInSignUp();
-                        }} />
-                </div>
 
-                {/*login popup*/}
-                <LoginPopup shown={this.state.loginPopupShown} />
-            </React.Fragment>
-        );
+        //if the user is not logged in
+        if (!this.context) {
+            return (
+                <React.Fragment>
+                    <PageHeader heading="Your account" subheading="Join us" />
+                    
+                    {/*why you need an account section*/}
+                    <div>
+                        <GenericMarkupSection
+                            heading="Make your account"
+                            paragraph="Create an account with us and unlock the full potential of Hunter PCs. We do not collect any of your personal information as privacy is one of our core values here."
+                            left={true}
+                            dividerLine={true}
+                            imagePath="images/gamingSetupWIDE2.jpeg"
+                            buttonText="Click here to sign up"
+                            buttonAction={() => {
+                                this.startLogInSignUp();
+                            }} />
+                    </div>
+    
+                    {/*login popup*/}
+                    <LoginPopup shown={this.state.loginPopupShown} />
+    
+                </React.Fragment>
+            );
+        }
+        else {
+            return (
+                <React.Fragment>
+                    <PageHeader heading="Your account" subheading="Welcome back!" />
+
+                    {/*your account section*/}
+                    <div>
+                        <GenericMarkupSection
+                            heading={`Hello, ${this.context.email}`}
+                            paragraph="Welcome back to your Hunter PCs account and thanks for being a member. You'll get special perks like discounts after checkout and more payment options. If you want to sign out or log into a different account, please use the below button."
+                            left={true}
+                            dividerLine={true}
+                            imagePath="images/gamingSetupWIDE2.jpeg"
+                            buttonText="Sign out"
+                            buttonAction={() => {
+                                const auth = firebaseInstance.getFirebaseAuth();
+                                signOut(auth).then(() => {
+                                    window.location.reload();
+                                });
+                            }} />
+                    </div>
+                </React.Fragment>
+            );
+        };
     };
 
     startLogInSignUp() {
