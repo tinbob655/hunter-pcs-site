e5c9651dce553ac091c34700cf9dd71abf8356da
Will Rutland <wrutland07@gmail.com>
2024-09-27 21:15:45 +0100

    added firebase auth context provider

---
+++ src/context/authContext.jsx
@@ -0,0 +1,42 @@
+import React, {Component} from 'react';
+import firebaseInstance from '../classes/firebase.js';
+import { onAuthStateChanged } from 'firebase/auth';
+
+const AuthContext = React.createContext(false);
+const auth = firebaseInstance.getFirebaseAuth();
+
+class AuthProvider extends Component {
+
+    constructor(props) {
+        super(props);
+
+        //this will store the firebase auth state
+        this.state = {
+            user: null,
+        };
+
+        //when the auth state changes, update state
+        onAuthStateChanged(auth, (user) => {
+            this.setState({
+                user: user,
+            });
+
+            console.log(user)
+        });
+    };
+
+    render() {
+        const {children} = this.props;
+        return (
+            <React.Fragment>
+                <AuthContext.Provider value={this.state.user}>
+                    {children}
+                </AuthContext.Provider>
+            </React.Fragment>
+        );
+    };
+};
+
+export {AuthProvider}
+export const AuthConsumer = AuthContext.Consumer;
+export default AuthContext;
--- src/index.js
+++ src/index.js
@@ -3,14 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.scss';
 import AllRoutes from './routes.jsx';
 import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from './context/authContext.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+    <AuthProvider>
 
       <AllRoutes />
-
+      
+    </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
