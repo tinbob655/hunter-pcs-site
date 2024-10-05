daf1eff60c60cc32b014e5042d64fb70d524040b
Will Rutland <wrutland07@gmail.com>
2024-10-05 14:55:01 +0100

    added vercel analytics and speed insights

--- src/index.js
+++ src/index.js
@@ -6,10 +6,14 @@ import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './context/authContext.jsx';
 import Header from './components/multiPageComponents/header.jsx';
 import ScrollToTop from './components/multiPageComponents/scrollToTop.jsx';
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Analytics/>
+    <SpeedInsights/>
     <BrowserRouter>
     <ScrollToTop />
     <AuthProvider>
