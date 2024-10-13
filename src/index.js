3a4798b6ef7c98ef89c37f113ec646a9098cf1dc
Will Rutland <wrutland07@gmail.com>
2024-10-13 10:31:35 +0100

    added footer

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import AllRoutes from './routes.jsx';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './context/authContext.jsx';
 import Header from './components/multiPageComponents/header.jsx';
+import Footer from './components/multiPageComponents/footer.jsx';
 import ScrollToTop from './components/multiPageComponents/scrollToTop.jsx';
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/react';
@@ -19,9 +20,10 @@ root.render(
     <AuthProvider>
 
       <Header />
-      <div style={{marginTop: '125px'}}>
+      <div style={{marginTop: '125px'}} id="pageContentWrapper">
       <AllRoutes />
       </div>
+      <Footer />
       
     </AuthProvider>
     </BrowserRouter>
