76d6aa020a2cf033ac7ff44d864322b370564cb6
Will Rutland <wrutland07@gmail.com>
2024-10-27 15:04:50 +0000

    setup mobile stylesheet

--- src/context/mobileContext.jsx
+++ src/context/mobileContext.jsx
@@ -13,17 +13,31 @@ class MobileProvider extends Component {
         this.state = {
             isMobile: initialIsMobile,
         };
+
+        //if the window was initially portrait, import the mobile stylesheet
+        if (initialIsMobile) {
+            import('../mobileStyles.scss');
+        };
     };
 
     componentDidMount() {
-        
         //update the mobile state when the window is resized
         window.onresize = () => {
+
             if (window.innerWidth >= window.innerHeight) {
-                this.setState({isMobile: false});
+
+                if (this.state.isMobile) {
+
+                    //width was greater than height, and current state is mobile
+                    window.location.reload();
+                };
             }
-            else {
+
+            else if (!this.state.isMobile) {
+
+                //width was less than height, and current state is desktop
                 this.setState({isMobile: true});
+                import('../mobileStyles.scss');
             };
         };
     };
