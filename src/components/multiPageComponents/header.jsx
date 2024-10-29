e79ed4b15b937e9e6c13ca7e54e8a801f6873954
Will Rutland <wrutland07@gmail.com>
2024-10-29 15:30:28 +0000

    added mobile header

--- src/components/multiPageComponents/header.jsx
+++ src/components/multiPageComponents/header.jsx
@@ -1,22 +1,86 @@
 import React, {Component} from 'react';
 import { Link } from 'react-router-dom';
+import MobileProvider from '../../context/mobileContext.jsx';
+import {Squash as Hamburger} from 'hamburger-react';
 
 class Header extends Component {
 
+    static contextType = MobileProvider;
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isMobile: this.context,
+            hamburgerOpen: false,
+        };
+    };
+
+    componentDidUpdate(prevProps, prevState) {
+
+        //if the mobile header is opened or closed
+        if (prevState.hamburgerOpen != this.state.hamburgerOpen) {
+            const header = document.getElementById('headerWrapper');
+            const hamburger = document.getElementById('hamburgerWrapper');
+            const delay = 75 //time in ms between header and hamburger sliding from side to side
+
+            if (this.state.hamburgerOpen) {
+
+                //hamburger has been opened
+                hamburger.classList.add('headerOpen');
+                setTimeout(() => {
+                    header.classList.add('headerOpen');
+                }, delay);
+            }
+            else {
+
+                //hamburger has been closed
+                header.classList.remove('headerOpen');
+                setTimeout(() => {
+                    hamburger.classList.remove('headerOpen');
+                }, delay);
+            };
+        };
+    };
+
     render() {
-        return(
-            <React.Fragment>
-                <div id="headerWrapper">
-                    <table style={{height: '100%'}}>
-                        <thead>
-                            <tr>
-                                {this.getHeaderCells()}
-                            </tr>
-                        </thead>
-                    </table>
-                </div>
-            </React.Fragment>
-        );
+
+        //desktop header
+        if (!this.state.isMobile) {
+            return(
+                <React.Fragment>
+                    <div id="headerWrapper">
+                        <table style={{height: '100%'}}>
+                            <thead>
+                                <tr>
+                                    {this.getHeaderCells()}
+                                </tr>
+                            </thead>
+                        </table>
+                    </div>
+                </React.Fragment>
+            );
+        }
+
+        //mobile header
+        else {
+            return (
+                <React.Fragment>
+                    <div id="headerWrapper">
+
+                        {/*actual header content*/}
+                        {this.getMobileHeaderCells()}
+                    </div>
+                    <div id="hamburgerWrapper">
+                        <Hamburger toggled={this.state.hamburgerOpen} toggle={() => {this.setState({hamburgerOpen: !this.state.hamburgerOpen})}} duration={0.25} color='#ffffff' rounded hideOutline={true} size={37} />
+                    </div>
+                </React.Fragment>
+            );
+        };
+    };
+
+    componentDidMount() {
+        this.setState({isMobile: this.context});
     };
 
     getHeaderCells() {
@@ -72,6 +136,38 @@ class Header extends Component {
         //update styles
         whiteLine.classList.remove('shown');
         cell.classList.remove('hovered')
+    };
+
+    getMobileHeaderCells() {
+
+        //generates HTML for the header buttons on mobile
+        let headerCellsHTML = [];
+        const pages = [
+            ['pcsMain', 'Gaming PCs'],
+            ['', 'Home'],
+            ['about', 'About'],
+            ['support', 'Support'],
+            ['tellAFriend', 'Tell a Friend'],
+            ['account', 'Account'],
+            ['basket', 'Basket'],
+        ];
+
+        //calculate the suitable margins for each button in the header so that it takes just less than 80% of the page height (less than 80% due to margins not being repeated at top and not applied at bottom)
+        const marginTopBottom = (Math.floor(80 / pages.length) /2).toString() + 'vh';
+
+        pages.forEach((page) => {
+            headerCellsHTML.push(
+                <React.Fragment>
+                    <Link to={page[0]} onClick={() => {this.setState({hamburgerOpen: false})}}>
+                        <h3 style={{marginTop: marginTopBottom, marginBottom: marginTopBottom}}>
+                            {page[1]}
+                        </h3>
+                    </Link>
+                </React.Fragment>
+            );
+        });
+
+        return headerCellsHTML;
     };
 };
 
